892511f33dc2074433977e4c77caecec94b5b488
Ethereal Squirrel <82890756+ethereal-squirrel@users.noreply.github.com>
2025-07-15 11:59:18 +0100

    Initial Calibre import support. (#2)

--- src/components/molecules/book/BookDescription.tsx
+++ src/components/molecules/book/BookDescription.tsx
@@ -6,10 +6,14 @@ export default function BookDescription({ book }: { book: Book }) {
   }
 
   return (
-    <div className="text-sm mb-[1rem] bg-primary rounded-xl p-3 text-white">
-      {book.metadata.description && book.metadata.description.length > 0
-        ? book.metadata.description
-        : "No description available."}
-    </div>
+    <div
+      className="text-sm mb-[1rem] bg-primary rounded-xl p-3 text-white"
+      dangerouslySetInnerHTML={{
+        __html:
+          book.metadata.description && book.metadata.description.length > 0
+            ? book.metadata.description
+            : "No description available.",
+      }}
+    />
   );
 }
